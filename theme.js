949d7897453716ba5eea751d271118157a9752e4
Lia <moua0061@yahoo.com>
2022-04-24 03:17:13 -0400

    fixed minor errors

--- theme.js
+++ theme.js
@@ -1,5 +1,3 @@
-console.log('Its working')
-
 let theme = localStorage.getItem('theme')
 
 if(theme == null){
@@ -14,7 +12,6 @@ let themeDots = document.getElementsByClassName('theme-dot')
 for (let i=0; i < themeDots.length; i++){
 	themeDots[i].addEventListener('click', function(){
 		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
 		setTheme(mode)
 	})
 }
